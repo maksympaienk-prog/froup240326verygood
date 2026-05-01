1f596b1970fa1af04522a5d30983124043546751
maksympaienk-prog <maksym.paienk@gmail.com>
2026-05-01 20:17:04 +0200

    Refactor main.py for clarity and formatting

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-# main.py
+
 import poems
 import sign
 
-# Ім'я автора
+
 author_name = "Олександр Іванов"
 
-# Вивід підпису через .format
+
 print(sign.SIGN_TEMPLATE.format(name=author_name))
 
-# Побажання одним рядком з \n
+
 wishes = "Більше практичних завдань\nЦікавих розборів коду"
 print(wishes)
 
-# Розділювач
 stars = "**************************************************************************"
 
-# Вивід віршів
+
 print(stars)
 print(poems.ZAPOVIT)
 print(stars)
